265ddbd4e61fea5f4eb598b158f61ab793426aa2
curso03 <tuemail@example.com>
2025-02-24 12:49:09 +0100

    proyecto creado

---
+++ src/main/java/es/santander/ascender/ejerc006/Ejerc006Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejerc006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerc006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerc006Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/santander/ascender/ejerc006/Ejerc006ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejerc006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejerc006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
