a146f11a92faf0f4c96aa000fc20aaced9178cc5
curso03 <tuemail@example.com>
2025-02-25 13:07:30 +0100

    Creado repository

---
+++ src/main/java/es/santander/ascender/ejerc006/repository/MesaRepository.java
@@ -0,0 +1,9 @@
+package es.santander.ascender.ejerc006.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.santander.ascender.ejerc006.model.Mesa;
+
+public interface MesaRepository extends JpaRepository<Mesa, Long>{
+
+}
---
+++ src/main/java/es/santander/ascender/ejerc006/repository/SillaRepository.java
@@ -0,0 +1,9 @@
+package es.santander.ascender.ejerc006.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.santander.ascender.ejerc006.model.Silla;
+
+public interface SillaRepository extends JpaRepository<Silla, Long>{
+
+}
